6449744ee6ae982bb616757f7b09e336d990d4f4
sabinahschmitz <sabinahschmitz@gmail.com>
2019-04-10 13:51:21 +0200

    Initital Framework

--- script.py
+++ script.py
@@ -1,2 +1,26 @@
 #ShoppingBasket
+INVENTORY = [["A0001", 12.99], ["A0002", 3.99], ...]
+
+class Basket: #Class
+    def __init__ (self, new, scan, total):
+        self.new = new_basket
+        self.scan = content_basket
+        self.total = total_basket
+    
+    def voucherone(self): #buyonegetonefree
+        if self.new == 3:
+            new_value = self.total =-1 
+            return new_value
+            #remove one
+        return self.total
+        else:
+            pass
+        
+    def rabatt(self): #10% Rabatt
+        if self.new == #total sum:
+            discount = 10 * self.total /100
+            new_total = self.total - discount
+            return new_total
+
+
 
